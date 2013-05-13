d320c7961a403f73cdf34c8288badd816cdf2f19
whdghks913 <whdghks913@hanmail.net>
2013-05-13 17:39:40 +0900

    Fix VR iosched ERROR

--- block/vr-iosched.c
+++ block/vr-iosched.c
@@ -26,6 +26,7 @@
 #include <linux/init.h>
 #include <linux/compiler.h>
 #include <linux/rbtree.h>
+#include <linux/version.h>
 
 #include <asm/div64.h>
 
@@ -41,8 +42,8 @@ BACKWARD,
 
 static const int sync_expire = HZ / 2; /* max time before a sync is submitted. */
 static const int async_expire = 5 * HZ; /* ditto for async, these limits are SOFT! */
-static const int fifo_batch = 16;
-static const int rev_penalty = 10; /* penalty for reversing head direction */
+static const int fifo_batch = 1;
+static const int rev_penalty = 1; /* penalty for reversing head direction */
 
 struct vr_data {
 struct rb_root sort_list;
@@ -72,14 +73,14 @@ return q->elevator->elevator_data;
 static void
 vr_add_rq_rb(struct vr_data *vd, struct request *rq)
 {
-struct request *alias = elv_rb_add(&vd->sort_list, rq);
-
-if (unlikely(alias)) {
-vr_move_request(vd, alias);
-alias = elv_rb_add(&vd->sort_list, rq);
-BUG_ON(alias);
-}
-
+//struct request *alias = elv_rb_add(&vd->sort_list, rq);
+//
+//if (unlikely(alias)) {
+//vr_move_request(vd, alias);
+//alias = elv_rb_add(&vd->sort_list, rq);
+//BUG_ON(alias);
+//}
+elv_rb_add(&vd->sort_list, rq);
 if (blk_rq_pos(rq) >= vd->last_sector) {
 if (!vd->next_rq || blk_rq_pos(vd->next_rq) > blk_rq_pos(rq))
 vd->next_rq = rq;
@@ -305,12 +306,14 @@ vr_move_request(vd, rq);
 return 1;
 }
 
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(2,6,38)
 static int
 vr_queue_empty(struct request_queue *q)
 {
 struct vr_data *vd = vr_get_data(q);
 return RB_EMPTY_ROOT(&vd->sort_list);
 }
+#endif
 
 static void
 vr_exit_queue(struct elevator_queue *e)
@@ -414,7 +417,9 @@ static struct elevator_type iosched_vr = {
 .elevator_merge_req_fn = vr_merged_requests,
 .elevator_dispatch_fn = vr_dispatch_requests,
 .elevator_add_req_fn = vr_add_request,
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(2,6,38)
 .elevator_queue_empty_fn = vr_queue_empty,
+#endif
 .elevator_former_req_fn = elv_rb_former_request,
 .elevator_latter_req_fn = elv_rb_latter_request,
 .elevator_init_fn = vr_init_queue,
