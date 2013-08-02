ef1b190f4a6626b8d8efc03d800044468e7a5438
whdghks913 <whdghks913@naver.com>
2013-08-02 17:58:07 +0900

    Prima_wlan

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Check git update list"
+git pull
+
 #######################################
 #    android_kernel_pantech_ef47s     #
 #######################################
@@ -47,6 +50,13 @@ fi
 # Finish - use build.sh
 fi
 
+# Prima_wlan
+if [ -f $FOLDER/drivers/staging/prima/wlan.ko ]; then
+	./bin/strip --strip-unneeded "$FOLDER/drivers/staging/prima/wlan.ko"
+	cp $FOLDER/net/wireless/cfg80211.ko .
+	cp $FOLDER/drivers/staging/prima/wlan.ko prima_wlan.ko
+fi
+
 # Copy zImage
 if [ -f $FOLDER/arch/arm/boot/zImage ]; then
     cp -f $FOLDER/arch/arm/boot/zImage ./
@@ -56,7 +66,7 @@ if [ -f $zImage ]; then
 	echo "--------------------------"
 	echo "     Makeing Boot.img"
 	echo "--------------------------"
-	./mkbootimg --cmdline "console=ttyHSL0,115200,n8 androidboot.hardware=qcom androidboot.carrier=SKT-KOR user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 maxcpus=2 loglevel=0" --base 0x80200000 --pagesize 2048 --kernel zImage --ramdisk ramdisk.gz --ramdiskaddr 0x82400000 -o boot.img
+	./bin/mkbootimg --cmdline "console=ttyHSL0,115200,n8 androidboot.hardware=qcom androidboot.carrier=SKT-KOR user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 maxcpus=2 loglevel=0" --base 0x80200000 --pagesize 2048 --kernel zImage --ramdisk ./bin/ramdisk.gz --ramdiskaddr 0x82400000 -o boot.img
 fi
 
 
@@ -70,3 +80,4 @@ fi
 if [ -d ../../out/target/$device ]; then
 cp -f boot.img ../../out/target/$device/boot.img
 fi
+
